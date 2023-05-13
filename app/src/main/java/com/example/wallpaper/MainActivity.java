55ec0e6ae5509e8f20b1e2d105cd29ccd4dfe7c9
AnugyaBajpai2454 <bajpaianugyaab@gmail.com>
2023-05-13 17:31:22 +0530

    Buttons

--- app/src/main/java/com/example/wallpaper/MainActivity.java
+++ app/src/main/java/com/example/wallpaper/MainActivity.java
@@ -6,6 +6,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
 
 import com.example.wallpaper.Adapters.CuratedAdapters;
@@ -13,6 +14,7 @@ import com.example.wallpaper.Listeners.CuratedResponseListener;
 import com.example.wallpaper.Listeners.OnRecyclerClickListener;
 import com.example.wallpaper.Models.CuratedApiResponse;
 import com.example.wallpaper.Models.Photo;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.List;
 
@@ -22,17 +24,42 @@ public class MainActivity extends AppCompatActivity implements OnRecyclerClickLi
     CuratedAdapters adapter;
     ProgressDialog dialog;
     RequestManager manager;
+    FloatingActionButton fab_next, fab_prev;
+    int page;
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fab_next = findViewById(R.id.fab_next);
+        fab_prev =findViewById(R.id.fab_prev);
         
         dialog= new ProgressDialog(this);
         dialog.setTitle("loading...");
 
         manager = new RequestManager(this);
         manager.getCuratedWallpapers(listener,"1");
+
+        fab_next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String next_page = String.valueOf(page+1);
+                manager.getCuratedWallpapers(listener, next_page);
+                dialog.show();
+
+            }
+        });
+        fab_prev.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(page>1){
+                    String prev_page = String.valueOf(page-1);
+                    manager.getCuratedWallpapers(listener, prev_page);
+                    dialog.show();
+                }
+            }
+        });
     }
      
     private  final CuratedResponseListener listener = new CuratedResponseListener() {
@@ -42,6 +69,7 @@ public class MainActivity extends AppCompatActivity implements OnRecyclerClickLi
                 Toast.makeText(MainActivity.this, "No Image Found!!", Toast.LENGTH_SHORT).show();
                 return;
             }
+            page = response.getPage();
             showData(response.getPhotos());
         }
 
